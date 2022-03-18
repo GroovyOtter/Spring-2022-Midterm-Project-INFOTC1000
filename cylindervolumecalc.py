37a6a39cf711befbdb6d6580a74375eaba452704
Kelly Sun <kellysun0124@gmail.com>
2022-03-17 20:21:15 -0500

    Add files via upload
    uploading code file in python

---
+++ cylindervolumecalc.py
@@ -0,0 +1,36 @@
+import math
+
+def input_error_from_user(prompt):
+    while (True):
+        try:
+            input_value = float(input(prompt))
+            if (input_value < 0):
+                print("Negative numbers are not accepted.  Please enter a positive number.")
+                continue
+            break
+        except ValueError:
+            print("Only numbers are accepted.  Please enter a number.")
+            continue
+    return input_value
+        
+def calculate_volume_of_cylinder():
+        radius = input_error_from_user("What is the radius of the cylinder? ")
+        height = input_error_from_user("What is the height of the cylinder? ")
+    
+        volume = math.pi * radius ** 2 * height
+        print("The volume of the cylinder is", volume)
+
+def main():
+    print("This program calculates the volume of a cylinder.")
+    while True:
+        calculate_volume_of_cylinder()
+        repeat = input("Would you like to perform another calculation? enter y/n ")
+        if (repeat == "y"):
+            print("")
+            continue
+        else:
+            break
+main()
+            
+
+
